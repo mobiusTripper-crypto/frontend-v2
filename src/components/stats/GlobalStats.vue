84981df1b352f27f023fe2fa103abce236e38d1e
danielmkm <danielbeets@protonmail.com>
2021-10-26 19:39:37 +0200

    add TVL back in

--- src/components/stats/GlobalStats.vue
+++ src/components/stats/GlobalStats.vue
@@ -2,12 +2,12 @@
   <div
     class="absolute right-4 top-2 float-right flex flex-col items-end hidden md:block"
   >
-<!--    <div-->
-<!--      v-if="tvl > 0 && beetsPrice > 0"-->
-<!--      class="text-green-500 font-semibold text-right"-->
-<!--    >-->
-<!--      TVL: ${{ fNum(tvl, 'usd_lg') }}-->
-<!--    </div>-->
+    <div
+      v-if="tvl > 0 && beetsPrice > 0"
+      class="text-green-500 font-semibold text-right"
+    >
+      TVL: ${{ fNum(tvl, 'usd_lg') }}
+    </div>
     <div v-if="beetsPrice > 0" class="text-red-500 font-semibold text-right">
       BEETS: {{ fNum(beetsPrice, 'usd') }}
     </div>
